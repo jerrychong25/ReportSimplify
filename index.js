e4b51a92d7782d65c1b7fee76a0a04f0eb45762e
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-09-06 23:09:22 +0800

    Added Firebase library

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import firebase from 'react-native-firebase';
+
+firebase.auth()
+  .signInAnonymouslyAndRetrieveData()
+  .then(credential => {
+    if (credential) {
+      console.log('default app user ->', credential.user.toJSON());
+    }
+  });
 
 AppRegistry.registerComponent(appName, () => App);
