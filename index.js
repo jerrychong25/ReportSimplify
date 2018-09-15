3aef03954881fe3a8effe689c9137b0d72b59c3d
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-09-15 22:36:54 +0800

    Added React Native Navigation library

--- index.js
+++ index.js
@@ -2,17 +2,22 @@
 
 // import {AppRegistry} from 'react-native';
 import { Navigation } from "react-native-navigation";
-import App from './App';
-import {name as appName} from './app.json';
+import {registerScreens} from './src/screens';
+
+// To remove
+// import App from './src/App';
+// import {name as appName} from './app.json';
+
+registerScreens();
 
 // AppRegistry.registerComponent(appName, () => App);
-Navigation.registerComponent(appName, () => App);
+// Navigation.registerComponent(appName, () => App);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
       component: {
-        name: appName
+        name: 'Initializing'
       }
     }
   });
