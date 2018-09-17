3f5bc3aeb1542255a77cf86c3a74d3eae508cb2e
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-09-18 00:14:07 +0800

    Removed ProfileScreen.js and EditScreenOne.js

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { Alert } from "react-native";
 import { Container, Header, Left, Body, Title, Card, CardItem, Content, Right, Icon, Button, Text } from "native-base";
-import { StackNavigator } from "react-navigation";
-import EditScreenOne from "./EditScreenOne.js";
-// import EditScreenTwo from "./EditScreenTwo.js";
-
-// From ProfileScreen index.js
-// import React, { Component } from "react";
-// import Profile from "./Profile.js";
-// import EditScreenOne from "./EditScreenOne.js";
-// import EditScreenTwo from "./EditScreenTwo.js";
-// import { StackNavigator } from "react-navigation";
 
 export default class Profile extends React.Component {
   componentDidMount() {
@@ -39,32 +29,3 @@ export default class Profile extends React.Component {
     );
   }
 }
-
-// Profile.navigationOptions = ({ navigation }) => ({
-//   header: (
-//     <Header>
-//       <Left>
-//         <Button transparent onPress={() => navigation.navigate("DrawerOpen")}>
-//           <Icon name="menu" />
-//         </Button>
-//       </Left>
-//       <Body>
-//         <Title>Profile</Title>
-//       </Body>
-//       <Right />
-//     </Header>
-//   )
-// });
-
-// // From ProfileScreen index.js
-// // export default (DrawNav = StackNavigator({
-// //   Profile: { screen: Profile },
-// //   EditScreenOne: { screen: EditScreenOne },
-// //   EditScreenTwo: { screen: EditScreenTwo }
-// // }));
-
-// export const DrawNav = StackNavigator({
-//   Profile: { screen: Profile },
-//   EditScreenOne: { screen: EditScreenOne },
-//   EditScreenTwo: { screen: EditScreenOne }
-// });
